4ab9ec145c0fa8a4b30924722140876347649cf5
Sujan Thapa <mrsujan06@gmail.com>
2021-04-09 23:20:06 +0100

    Created Welcome Screen

---
+++ app/src/main/java/com/example/bloom/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.bloom
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bloom.ui.theme.BloomTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BloomTheme {
+                WelcomeScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BloomTheme {
+        WelcomeScreen()
+
+    }
+}
---
+++ app/src/main/java/com/example/bloom/WelcomeScreen.kt
@@ -0,0 +1,163 @@
+package com.example.bloom
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bloom.ui.theme.BloomTheme
+import com.example.bloom.ui.theme.Pink900
+import com.example.bloom.ui.theme.White
+
+@Composable
+fun WelcomeScreen() {
+    WelcomeScreenContent()
+}
+
+@Composable
+private fun WelcomeScreenContent() {
+    val isLight = MaterialTheme.colors.isLight
+
+    Surface(color = MaterialTheme.colors.primary, modifier = Modifier.fillMaxSize()) {
+
+        WelcomeScreenBackground(isLight = isLight)
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            Spacer(modifier = Modifier.padding(72.dp))
+
+            LeafImage(isLight = isLight)
+
+            Spacer(modifier = Modifier.padding(48.dp))
+
+            LogoImage(isLight = isLight)
+
+            AppDescription()
+
+            CreateAccountButton()
+
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            LoginButton(isLight = isLight)
+
+        }
+
+    }
+}
+
+@Composable
+private fun WelcomeScreenBackground(isLight: Boolean) {
+
+    val imageRes = if (isLight) {
+        R.drawable.ic_light_welcome_bg
+    } else {
+        R.drawable.ic_dark_welcome_bg
+    }
+
+    Image(
+        painterResource(id = imageRes),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.FillBounds
+    )
+}
+
+@Composable
+private fun LeafImage(isLight: Boolean) {
+    val leafImage = if (isLight) {
+        R.drawable.ic_light_welcome_illos
+    } else {
+        R.drawable.ic_dark_welcome_illos
+    }
+
+    Image(
+        painter = painterResource(id = leafImage),
+        contentDescription = stringResource(R.string.leaf_image),
+        modifier = Modifier.offset(x = 88.dp)
+    )
+}
+
+@Composable
+private fun LogoImage(isLight: Boolean) {
+    val logoImage = if (isLight) {
+        R.drawable.ic_light_logo
+    } else {
+        R.drawable.ic_dark_logo
+    }
+
+    Image(
+        painter = painterResource(id = logoImage),
+        contentDescription = stringResource(R.string.app_logo)
+    )
+}
+
+@Composable
+private fun AppDescription() {
+    Text(
+        style = MaterialTheme.typography.subtitle1,
+        text = stringResource(R.string.app_description),
+        modifier = Modifier
+            .paddingFromBaseline(top = 32.dp, bottom = 40.dp)
+    )
+}
+
+@Composable
+private fun CreateAccountButton() {
+    Button(
+        shape = MaterialTheme.shapes.medium,
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary),
+        onClick = {/*TODO*/ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 16.dp)
+
+    ) {
+        Text(text = stringResource(R.string.create_account))
+    }
+}
+
+@Composable
+private fun LoginButton(isLight: Boolean) {
+
+    val loginColor = if (isLight) {
+        Pink900
+    } else {
+        White
+    }
+
+    TextButton(onClick = {/*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = stringResource(R.string.login),
+            color = loginColor
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewWelcomeDarkScreen() {
+    BloomTheme(darkTheme = true) {
+        WelcomeScreen()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewWelcomeLightScreen() {
+    BloomTheme(darkTheme = false) {
+        WelcomeScreen()
+    }
+}
+
+
